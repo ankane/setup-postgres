dca9e1a6f479df5d25889a7789ac66e52db7f26b
Andrew Kane <andrew@chartkick.com>
2020-11-10 20:31:31 -0800

    Use pg_ctl

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ if (![13, 12, 11, 10, 9.6].includes(postgresVersion)) {
 }
 
 if (process.platform == 'darwin') {
+  const bin = '/usr/local/opt/postgresql@' + postgresVersion + '/bin';
   if (postgresVersion != 13) {
     // run('brew remove postgresql');
-    run('rm -rf /usr/local/var/postgres');
+    // run('rm -rf /usr/local/var/postgres');
     run('brew install postgresql@' + postgresVersion);
-    run('/usr/local/opt/postgresql@' + postgresVersion + '/bin/initdb --locale=C -E UTF-8 /usr/local/var/postgres');
+    run(bin + '/initdb --locale=C -E UTF-8 /usr/local/var/postgres@' + postgresVersion);
   }
-  run('brew services start postgresql@' + postgresVersion);
-  run('echo "/usr/local/opt/postgresql@' + postgresVersion + '/bin" >> $GITHUB_PATH');
+  // run('brew services start postgresql@' + postgresVersion);
+  run(bin + '/pg_ctl -D /usr/local/var/postgres@12 start');
+  run('echo "' + bin + '" >> $GITHUB_PATH');
   run('sleep 10');
   run('cat /usr/local/var/log/postgres.log')
   run('ps aux | grep postgres');
