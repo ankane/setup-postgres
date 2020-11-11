db08f1522f0d3b2c3d307621376de1ae8ea429d0
Andrew Kane <andrew@chartkick.com>
2020-11-11 01:04:53 -0800

    Cleaner code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const execSync = require("child_process").execSync;
+const execSync = require('child_process').execSync;
 
 function run(command) {
   console.log(command);
@@ -8,23 +8,23 @@ function run(command) {
 const postgresVersion = parseFloat(process.env['INPUT_POSTGRES-VERSION'] || 13);
 
 if (![13, 12, 11, 10, 9.6].includes(postgresVersion)) {
-  throw 'Invalid Postgres version: ' + postgresVersion;
+  throw `Postgres version not supported: ${postgresVersion}`;
 }
 
 if (process.platform == 'darwin') {
-  const bin = '/usr/local/opt/postgresql@' + postgresVersion + '/bin';
+  const bin = `/usr/local/opt/postgresql@${postgresVersion}/bin`;
   let dataDir = '/usr/local/var/postgres';
   if (postgresVersion != 13) {
-    dataDir = dataDir + '@' + postgresVersion;
-    run('brew install postgresql@' + postgresVersion);
-    run(bin + '/initdb --locale=C -E UTF-8 ' + dataDir);
+    dataDir += `@${postgresVersion}`;
+    run(`brew install postgresql@${postgresVersion}`);
+    run(`${bin}/initdb --locale=C -E UTF-8 ${dataDir}`);
   }
-  run(bin + '/pg_ctl -D ' + dataDir + ' start');
+  run(`${bin}/pg_ctl -D ${dataDir} start`);
 } else {
   if (postgresVersion != 13) {
-    run('sudo pg_dropcluster 13 main');
-    run('sudo apt-get install postgresql-' + postgresVersion);
+    run(`sudo pg_dropcluster 13 main`);
+    run(`sudo apt-get install postgresql-${postgresVersion}`);
   }
-  run('sudo systemctl start postgresql@' + postgresVersion + '-main');
-  run('sudo -u postgres createuser -s $USER');
+  run(`sudo systemctl start postgresql@${postgresVersion}-main`);
+  run(`sudo -u postgres createuser -s $USER`);
 }
