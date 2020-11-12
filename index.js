6649f357e9ca21fc5d4be0542dd95fa99d4366b2
Andrew Kane <andrew@chartkick.com>
2020-11-12 02:37:19 -0800

    Added pg_stat_statements to shared_preload_libraries

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const appendFileSync = require('fs').appendFileSync;
 const execSync = require('child_process').execSync;
+const fs = require('fs');
+const path = require('path');
 
 function run(command) {
   console.log(command);
   execSync(command, {stdio: 'inherit'});
+}
+
+function addToPath(newPath) {
+  fs.appendFileSync(process.env.GITHUB_PATH, newPath);
+}
+
+function isMac() {
+  return process.platform == 'darwin';
+}
+
+function isWindows() {
+  return process.platform == 'win32';
+}
+
+function enablePgStatStatements(dir) {
+  const file = path.join(dir, 'postgresql.conf');
+  const contents = `shared_preload_libraries = 'pg_stat_statements'\n`;
+
+  if (isMac() || isWindows()) {
+    fs.appendFileSync(file, contents);
+  } else {
+    run(`echo "${contents}" | sudo tee -a ${file}`);
+  }
 }
 
 const postgresVersion = parseFloat(process.env['INPUT_POSTGRES-VERSION'] || 13);
@@ -12,7 +36,7 @@ if (![13, 12, 11, 10, 9.6].includes(postgresVersion)) {
   throw `Postgres version not supported: ${postgresVersion}`;
 }
 
-if (process.platform == 'darwin') {
+if (isMac()) {
   const bin = `/usr/local/opt/postgresql@${postgresVersion}/bin`;
   let dataDir = '/usr/local/var/postgres';
 
@@ -26,22 +50,24 @@ if (process.platform == 'darwin') {
     run(`${bin}/initdb --locale=C -E UTF-8 ${dataDir}`);
   }
 
+  enablePgStatStatements(dataDir);
+
   // start
   run(`${bin}/pg_ctl -D ${dataDir} start`);
 
-  // set path
-  appendFileSync(process.env.GITHUB_PATH, bin);
-} else if (process.platform == 'win32') {
+  addToPath(bin);
+} else if (isWindows()) {
   if (postgresVersion != 13) {
     throw `Postgres version not supported on Windows: ${postgresVersion}`;
   }
+
+  enablePgStatStatements(process.env.PGDATA);
 
   // start
   run(`sc config postgresql-x64-13 start=auto`);
   run(`net start postgresql-x64-13`);
 
-  // set path
-  appendFileSync(process.env.GITHUB_PATH, process.env.PGBIN);
+  addToPath(process.env.PGBIN);
 } else {
   if (postgresVersion != 13) {
     // remove previous cluster so port 5432 is used
@@ -50,6 +76,8 @@ if (process.platform == 'darwin') {
     // install new version
     run(`sudo apt-get install postgresql-${postgresVersion}`);
   }
+
+  enablePgStatStatements(`/etc/postgresql/${postgresVersion}/main`);
 
   // start
   run(`sudo systemctl start postgresql@${postgresVersion}-main`);
