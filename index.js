5ead5280369d761298b17fc12c526586712dc8e9
Andrew Kane <andrew@ankane.org>
2026-01-20 07:57:23 -0800

    Test ubuntu-slim

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ if (isMac()) {
     spawnSync(`sudo`, [`tee`, `/etc/apt/sources.list.d/pgdg.list`], {input: pgdgList});
   }
 
-  const slim = process.env['ImageOS'] === 'undefined';
+  const slim = !process.env['ImageOS'];
 
   if (postgresVersion != defaultVersion || isArm() || slim) {
     // remove previous cluster so port 5432 is used
